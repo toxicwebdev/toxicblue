d4b6135e657735fe6d45bc653aa75b8c5262731f
ToXic Web <37346569+toxicwebdev@users.noreply.github.com>
2025-04-30 21:23:33 +0300

    fix: build failure caused by docker.repo

--- files/scripts/adddockerrepo.sh
+++ files/scripts/adddockerrepo.sh
@@ -4,4 +4,4 @@
 set -oue pipefail
 
 rpm --import https://download.docker.com/linux/fedora/gpg
-echo -e "[docker-ce-stable]\nname=Docker CE Stable - $basearch\nbaseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/fedora/gpg\n\n[docker-ce-testing]\nname=Docker CE Testing - $basearch\nbaseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/test\nenabled=0\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/fedora/gpg" | sudo tee /etc/yum.repos.d/docker-ce.repo > /dev/null
+echo -e "[docker-ce-stable]\nname=Docker CE Stable - x86_64\nbaseurl=https://download.docker.com/linux/fedora/$releasever/x86_64/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/fedora/gpg\n\n[docker-ce-testing]\nname=Docker CE Testing - x86_64\nbaseurl=https://download.docker.com/linux/fedora/$releasever/x86_64/test\nenabled=0\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/fedora/gpg" | sudo tee /etc/yum.repos.d/docker-ce.repo > /dev/null
