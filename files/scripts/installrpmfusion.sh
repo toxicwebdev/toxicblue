bdd9f20721c4a0042869fdbad4b22be6ebbe6fa9
toxicwebdev <toxicwebdev@gmail.com>
2025-05-07 22:56:22 +0300

    fix: build failure caused rpmfusion

--- files/scripts/installrpmfusion.sh
+++ files/scripts/installrpmfusion.sh
@@ -3,10 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-#rpm-ostree install \
-#    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
-#    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
-
 rpm-ostree install \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
